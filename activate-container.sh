bf17781a13ed49d4404adc1c73ef3fd5f8051bd1
Paul Fauth-Mayer <paulfauthmayer@gmail.com>
2021-12-13 16:54:41 +0100

    Enable persistent bash history in docker

--- activate-container.sh
+++ activate-container.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 CONTAINERNAME=nemoco-container
+SCRIPT_PATH=$(dirname $(realpath -s $0))
 
 read -p "If this container exists already, it will be overwritten. Are you sure?
 [yY/nN] : " -n 1 -r
@@ -9,10 +10,18 @@ then
     docker rm $CONTAINERNAME
 fi
 
+# create bash history for docker container
+BASH_HISTORY=".docker_bash_history"
+if ! test -f "$BASH_HISTORY"; then
+    touch $BASH_HISTORY
+fi
+
+# start the container
 docker run \
     --name $CONTAINERNAME \
     -it \
     -p 8889:8889 \
     --gpus all \
     --mount type=bind,source=/home/paul/repos/nemoco-tf,target=/code \
+    -v $SCRIPT_PATH/$BASH_HISTORY:/root/.bash_history \
     nemoco:latest
